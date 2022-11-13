382eb597764e731dfad34c66df8277aa19f035ba
Garrett <colliergc@hendrix.edu>
2022-11-12 18:28:10 -0600

    Remove unneeded imports and update deprecated code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+// NOT USED AS AN IMPORT
+// import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:to_dont_list/main.dart';
 import 'package:to_dont_list/to_do_items.dart';
-import 'package:to_dont_list/astra.dart';
+
+// NOT USED AS AN IMPORT
+// import 'package:to_dont_list/astra.dart';
 
 void main() {
   test('Item abbreviation should be first letter', () {
